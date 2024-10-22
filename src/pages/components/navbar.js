0f2ab5fe71a11e452ef7622f58e7798f28a1eac9
Sabashibr <160284018+Sabashibr@users.noreply.github.com>
2024-10-22 14:26:27 -0300

    Add files via upload

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -1,4 +1,19 @@
-function Navbar() {
-    return <h1>Navbar</h1>
+import {Container, Nav, Navbar} from 'react-bootstrap';
+import style from "@/styles/Navbar.module.css"
+
+export default function Menu() {
+  return (
+  <Container>
+    <Navbar expand="sm" bg="dark" className={`${style.degrademenu} p-2 border border-2 border-primary rounded-2`}>
+        <Navbar.Brand href="/">6V</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/lista">Lista</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+  </Container>
+  );
 }
-export default Navbar;
